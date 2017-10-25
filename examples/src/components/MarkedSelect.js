7d62a40cde9dbf7c4477e41e2c6bd3d813707534
Riza Nugraha <rnugraha@gmail.com>
2017-10-25 19:28:02 +0200

    Initialise add all and clear selection buttons

--- examples/src/components/MarkedSelect.js
+++ examples/src/components/MarkedSelect.js
@@ -8,6 +8,7 @@ import React from 'react';
 import createClass from 'create-react-class';
 import PropTypes from 'prop-types';
 import Select from 'react-select';
+import classNames from 'classnames';
 import stripDiacritics from './stripDiacritics';
 
 const MarkedValue = createClass({
@@ -58,7 +59,53 @@ const MarkedOption = createClass({
 				 onMouseMove={this.handleMouseMove}
 				 title={this.props.option.title}>
 				{this.renderMarker()}
-				{this.props.children}
+				{this.props.option.label}
+			</div>
+		);
+	}
+});
+
+const OptionTools = createClass({
+	propTypes: {
+		onAddAll: PropTypes.func,
+		onClearAll: PropTypes.func,
+	},
+
+	addAll(event) {
+		event.preventDefault();
+		event.stopPropagation();
+		this.props.onAddAll();
+	},
+
+	clearAll(event) {
+		event.preventDefault();
+		event.stopPropagation();
+		this.props.onClearAll();
+	},
+
+	render () {
+		let optionToolsStyles = {
+			fontColor: 'inherit',
+			textAlign:'center',
+			padding: '1em',
+			borderBottom: '1px solid #ccc'
+		};
+		let toolButtonStyle = {
+			borderRadius: '0.3em',
+			fontColor: 'inherit',
+			backgroundColor: 'white',
+			border: '1px solid #ccc',
+			padding: '0.5em',
+			textAlign: 'center',
+			textDecoration: 'none',
+			fontSize: 'smaller',
+			cursor: 'pointer',
+			marginLeft: '0.3em',
+		};
+		return (
+			<div style={optionToolsStyles}>
+				<button onClick={this.addAll} style={toolButtonStyle}> Select all</button>
+				<button onClick={this.clearAll} style={toolButtonStyle}>Clear</button>
 			</div>
 		);
 	}
@@ -79,7 +126,9 @@ const MarkedSelectField = createClass({
 
 		if (currentValue) currentValue = currentValue.map(i => i[this.props.valueKey]);
 
-		return options.filter(option => {
+		this._visibleOptions = options.filter(option => {
+
+			// mark as selected when options are selected
 			if (currentValue && currentValue.indexOf(option[this.props.valueKey]) > -1) {
 				option.isSelected = true;
 			}
@@ -99,10 +148,11 @@ const MarkedSelectField = createClass({
 				(this.props.matchProp !== 'label' && valueTest.substr(0, filterValue.length) === filterValue) ||
 				(this.props.matchProp !== 'value' && labelTest.substr(0, filterValue.length) === filterValue)
 			) : (
-				(this.props.matchProp !== 'la	bel' && valueTest.indexOf(filterValue) >= 0) ||
+				(this.props.matchProp !== 'label' && valueTest.indexOf(filterValue) >= 0) ||
 				(this.props.matchProp !== 'value' && labelTest.indexOf(filterValue) >= 0)
 			);
 		});
+		return this._visibleOptions;
 	},
 
 	getInitialState () {
@@ -154,6 +204,100 @@ const MarkedSelectField = createClass({
 		return null;
 	},
 
+	clearIsSelectedFlags() {
+		this.props.options.map( option => {
+			option.isSelected = false;
+			return option;
+		});
+	},
+
+	getResetValue () {
+		if (this.props.resetValue !== undefined) {
+			return this.props.resetValue;
+		} else if (this.props.multi) {
+			return [];
+		} else {
+			return null;
+		}
+	},
+
+	setValue (value) {
+		if (this.props.autoBlur) {
+			this.blurInput();
+		}
+		if (this.props.required) {
+			const required = this.handleRequired(value, this.props.multi);
+			this.setState({ required });
+		}
+		if (this.props.onChange) {
+			if (this.props.simpleValue && value) {
+				value = this.props.multi ?
+					value.map(i => i[this.props.valueKey]).join(this.props.delimiter) : value[this.props.valueKey];
+			}
+			this.props.onChange(value);
+		}
+	},
+
+	handleAddAll () {
+		this._visibleOptions.forEach( visibleOption => {
+			this.props.options.map( option => {
+				if ( option.value === visibleOption.value ) {
+					option.isSelected = true;
+				}
+			});
+		});
+		this.setValue(this._visibleOptions);
+		this.setState({ value:this._visibleOptions });
+	},
+
+	handleClearAll () {
+		this.setValue(this.getResetValue());
+		this.clearIsSelectedFlags();
+		this.setState({ value:[] });
+	},
+
+	renderMenu (params) {
+		return params.options.map((option, i) => {
+			let isSelected = params.valueArray && params.valueArray.indexOf(option) > -1;
+			let isFocused = option === params.focusedOption;
+			let optionClass = classNames(params.optionClassName, {
+				'Select-option': true,
+				'is-selected': isSelected,
+				'is-focused': isFocused,
+				'is-disabled': option.disabled,
+			});
+			if (i === 0 ) {
+				return (
+					<div key={`option-${i}-${option[params.valueKey]}`}>
+						<OptionTools
+							onAddAll={this.handleAddAll}
+							onClearAll={this.handleClearAll}
+						/>
+						<MarkedOption
+							className={optionClass}
+							option={option}
+							onFocus={params.onFocus}
+							onSelect={params.onSelect}
+							key={`option-${i}-${option[params.valueKey]}`}
+							children={params.children}
+						/>
+					</div>
+				);
+			}
+			return (
+				<div key={`option-${i}-${option[params.valueKey]}`}>
+					<MarkedOption
+						className={optionClass}
+						option={option}
+						onFocus={params.onFocus}
+						onSelect={params.onSelect}
+						children={params.children}
+					/>
+				</div>
+			);
+		});
+	},
+
 	render () {
 		const { value } = this.state;
 		return (
@@ -165,6 +309,7 @@ const MarkedSelectField = createClass({
 					disabled={this.props.disabled}
 					multi
 					onChange={this.toggleSelection}
+					menuRenderer={this.renderMenu}
 					optionComponent={MarkedOption}
 					options={this.props.options}
 					valueComponent={MarkedValue}
